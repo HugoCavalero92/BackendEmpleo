54c1791b2ab7896440274eb4e797944d4adca98e
Hugo Cavalero <cavalerohugo92@gmail.com>
2022-06-23 20:42:57 -0300

    Añadimos el Front End

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 
+
 const app = express();
 
 // settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3300);
 
 // middlewares 
 app.use(cors());
